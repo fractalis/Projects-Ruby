dd7507549b3994fdc588c811e5b6ad3aa34fff95
Scott Rallya <scott.rallya@mongoosemetrics.com>
2014-11-04 14:41:21 -0500

    One-liner factorial and tail-recursive approach

--- Numbers/factorial.rb
+++ Numbers/factorial.rb
@@ -1,4 +1,5 @@
-def factorial_loop(x)
+# Iterative
+def factorial_l(x)
 
     tot = 1
     while(x > 0)
@@ -8,10 +9,25 @@ def factorial_loop(x)
     return tot
 end
 
-def factorial_recurse(x)
+# Recursive
+def factorial_r(x)
     if(x==1)
         return x
     else
-        return x*factorial_recurse(x-1)
+        return x*factorial_r(x-1)
     end
+end
+
+# Recursive one-liner
+def factorial_rol(x)
+    x.zero? ? 1 : x*factorial_rol(x-1)
+end
+
+# Use tail-recursion
+def factorial_tr(x)
+    def factorial_tr_impl(x, memo)
+        x.zero? ? memo : factorial_tr_impl(x-1, x*memo)
+    end
+
+    return factorial_tr_impl(x, 1)
 end
